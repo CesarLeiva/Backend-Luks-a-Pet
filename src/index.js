23e51f43b47c5e8e8938c06585718a1d2a800a49
Luis <luissolivaresp@gmail.com>
2022-11-21 16:49:14 -0500

    actualizar origin

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://luks-a-pet-production.up.railway.app"],
+    origin: ["http://127.0.0.1:5500", "https://luks-a-pet-production.up.railway.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
